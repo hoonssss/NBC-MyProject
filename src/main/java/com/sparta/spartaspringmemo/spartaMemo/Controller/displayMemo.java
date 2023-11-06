8b4408667a5268864da59447b6ce1f49ee07ea22
JaeHoon <124040137+hoonssss@users.noreply.github.com>
2023-11-06 03:42:53 +0000

    Update displayMemo.java

--- src/main/java/com/sparta/spartaspringmemo/spartaMemo/Controller/displayMemo.java
+++ src/main/java/com/sparta/spartaspringmemo/spartaMemo/Controller/displayMemo.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class displayMemo {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String display(){
         return "memo";
     }
