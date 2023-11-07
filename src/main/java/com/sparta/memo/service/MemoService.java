05c43901ce4779d14aa5ef87059911a1ac63c9a7
JaeHoonYun <wogns8030@naver.com>
2023-11-07 12:14:00 +0900

    jdbc -> Spring Data JPA

--- src/main/java/com/sparta/memo/service/MemoService.java
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -5,8 +5,10 @@ import com.sparta.memo.dto.MemoResponseDto;
 import com.sparta.memo.entity.Memo;
 import com.sparta.memo.repository.MemoRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
 @Service
 public class MemoService {
 
@@ -21,7 +23,7 @@ public class MemoService {
 //        this.memoRepository = memoService;
 //    }
 
-    private MemoService(MemoRepository memoRepository){
+    public MemoService(MemoRepository memoRepository){
         this.memoRepository = memoRepository;
     }
 
@@ -39,37 +41,51 @@ public class MemoService {
         Memo saveMemo = memoRepository.save(memo);
 
         // Entity -> ResponseDto
-        MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
+        MemoResponseDto memoResponseDto = new MemoResponseDto(saveMemo);
 
         return memoResponseDto;
     }
 
     public List<MemoResponseDto> getMethod() {
 //        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
-        return memoRepository.findAll();
+        return memoRepository.findAll()
+                .stream()
+                .map(MemoResponseDto::new)
+                .toList(); //stream에서 memo가 하나씩 빠져나가고 mpa에 의해서 변환이 될건데 MemoResponseDto에 생성자 중에
+                            // memo를 파라미터로 가지고 있는 생성자가 호출이 되고 그게 하나씩 변환이 되면서 그 값들을 List로 바꿔준다
     }
 
+    @Transactional //변경감지를 위해 달아줘야함
     public Long putMethod(Long id, MemoRequestDto requestDto) {
 //        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
+//        해당 id조회
+//        Optional<Memo> memo = memoRepository.findById(id);
+        Memo memo = memoRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("메모가없음") //null값일때
+        );
+        memo.update(requestDto);
 
-        Memo memo = memoRepository.findById(id);
-        if(memo != null) {
-            memoRepository.update(id, requestDto);
-            return id;
-        } else {
-            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
-        }
+        return id;
     }
 
     public Long deleteMethod(Long id) {
         // 해당 메모가 DB에 존재하는지 확인
 //        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
-        Memo memo = memoRepository.findById(id);
-        if(memo != null) {
-            memoRepository.delete(id);
-            return id;
-        } else {
-            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
-        }
+//        Memo memo = memoRepository.findById(id);
+//        if(memo != null) {
+//            memoRepository.delete(id);
+//            return id;
+//        } else {
+//            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+//        }
+        Memo memo = findMemo(id);
+        memoRepository.delete(memo);
+        return id;
+    }
+
+    private Memo findMemo(Long id){
+        return  memoRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("메모가없음") //null값일때
+        );
     }
 }
