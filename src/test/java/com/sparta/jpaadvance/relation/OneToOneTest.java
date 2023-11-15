fe7ca9e04ae3153b99d52a7fe20ff515a837f41f
JaeHoonYun <wogns8030@naver.com>
2023-11-15 14:10:23 +0900

    @OneToOne 단방향테스트

---
+++ src/test/java/com/sparta/jpaadvance/relation/OneToOneTest.java
@@ -0,0 +1,41 @@
+package com.sparta.jpaadvance.relation;
+
+import com.sparta.jpaadvance.entity.Food;
+import com.sparta.jpaadvance.entity.User;
+import com.sparta.jpaadvance.repository.FoodRepository;
+import com.sparta.jpaadvance.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional//insert,delete,update..
+public class OneToOneTest {
+
+    @Autowired
+    FoodRepository foodRepository;
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    @Rollback(value = false) //test에서는 Transactional에 의해 자동 rollback 됨으로 false설정
+    @DisplayName("1대1 단방향 테스트")
+    void test1(){
+
+        User user = new User();
+        user.setName("Robbie");
+
+        Food food = new Food();
+        food.setName("후라이드 치킨");
+        food.setPrice(15000);
+        food.setUser(user); //외래키(연관 관계)설정
+
+        userRepository.save(user);
+        foodRepository.save(food);
+    }
+
+
+}
