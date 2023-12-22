3baffa83fe976ebdbb490b1243c965c55ec0f905
JaeHoon <wogns8030@naver.com>
2023-12-22 10:41:31 +0900

    fix : QueryDsl

--- src/main/java/com/spartanullnull/otil/domain/user/repository/UserRepository.java
+++ src/main/java/com/spartanullnull/otil/domain/user/repository/UserRepository.java
@@ -9,12 +9,16 @@ import org.springframework.stereotype.*;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>, QuerydslPredicateExecutor<User>{
 
+    @Query("select u from User u where u.accountId = :accountId")
     Optional<User> findByAccountId(String accountId);
 
+    @Query("select u from User u where u.email = :email")
     Optional<User> findByEmail(String email);
-    
+
+    @Query("select u from User u where u.nickname = :nickname")
     Optional<User> findByNickname(String nickname);
 
+    @Query("select u from User u where u.kakaoId = :kakaoId")
     Optional<User> findByKakaoId(Long kakaoId);
 
     Optional<User> findByAccountIdAndPassword(String accountId, String encode);
