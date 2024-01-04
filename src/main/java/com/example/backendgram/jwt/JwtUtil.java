df6514b46fb25fb963fd1c14baeeb6d9b018da4b
JaeHoon <wogns8030@naver.com>
2024-01-04 10:14:38 +0900

    fix : jwt

--- src/main/java/com/example/backendgram/jwt/JwtUtil.java
+++ src/main/java/com/example/backendgram/jwt/JwtUtil.java
@@ -6,14 +6,19 @@ import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
+import org.springframework.web.context.WebApplicationContext;
 
 @Slf4j(topic = "JwtUtil")
 @Component
@@ -27,15 +32,38 @@ public class JwtUtil {
     // 토큰 만료시간
     private final long TOKEN_TIME = 60 * 60 * 1000L; // 60분
 
+    private final long REFRESH_TOKEN_EXPIRATION_TIME = 60 * 60 * 2000L; //120분
+
+    @Autowired
+    private TokenStore tokenStore;
+
+    @Autowired
+    @Lazy
+    private WebApplicationContext applicationContext;
+
+    private UserDetailsService getUserDetailsService() {
+        return applicationContext.getBean(UserDetailsService.class);
+    }
+
+
     @Value("${jwt.secret.key}") // Base64 Encode 한 SecretKey
     private String secretKey;
     private Key key;
     private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
+    @Value("${jwt.refresh.token.secret.key}")
+    private String refreshSecretKey;
+    private Key refreshKey;
+    private final SignatureAlgorithm refreshSignatureAlgorithm = SignatureAlgorithm.HS256;
+
+
     @PostConstruct
     public void init() {
         byte[] bytes = Base64.getDecoder().decode(secretKey);
         key = Keys.hmacShaKeyFor(bytes);
+
+        byte[] refreshBytes = Base64.getDecoder().decode(refreshSecretKey);
+        refreshKey = Keys.hmacShaKeyFor(refreshBytes);
     }
 
     // 토큰 생성
@@ -50,6 +78,21 @@ public class JwtUtil {
                         .setIssuedAt(date) // 발급일
                         .signWith(key, signatureAlgorithm) // 암호화 알고리즘
                         .compact();
+    }
+
+    public String generateRefreshToken() {
+        Date date = new Date();
+        return BEARER_PREFIX +
+            Jwts.builder()
+                .setId(UUID.randomUUID().toString())
+                .setIssuedAt(date)
+                .setExpiration(new Date(date.getTime() + REFRESH_TOKEN_EXPIRATION_TIME))
+                .signWith(refreshKey, refreshSignatureAlgorithm)
+                .compact();
+    }
+
+    public Jws<Claims> parseRefreshToken(String refreshToken) {
+        return Jwts.parserBuilder().setSigningKey(refreshKey).build().parseClaimsJws(refreshToken);
     }
 
     // header 에서 JWT 가져오기
@@ -78,16 +121,47 @@ public class JwtUtil {
         return false;
     }
 
+    public Tokens refreshTokens(String refreshToken) {
+        try {
+            Jws<Claims> refreshClaims = parseRefreshToken(refreshToken);
+            String username = refreshClaims.getBody().getSubject();
+
+            if (tokenStore.isValidRefreshToken(username, refreshToken)) {
+                UserDetailsImpl userDetails = (UserDetailsImpl) getUserDetailsService().loadUserByUsername(username);
+                Tokens tokens = generateTokens(userDetails);
+                tokenStore.updateRefreshTokenExpiration(username, new Date(System.currentTimeMillis() + REFRESH_TOKEN_EXPIRATION_TIME));
+                return tokens;
+            } else {
+                throw new RuntimeException("Refresh token 유효하지 않습니다..");
+            }
+        } catch (JwtException e) {
+            throw new RuntimeException("Refresh token 실패하였습니다..", e);
+        }
+    }
+
     // 토큰에서 사용자 정보 가져오기
     public Claims getUserInfoFromToken(String token) {
         return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
     }
 
-    //username, role 확인 후 createToken 생성
-    public String generateToken(UserDetailsImpl userDetails) {
+    public Tokens generateTokens(UserDetailsImpl userDetails) {
         String username = userDetails.getUsername();
         UserRoleEnum role = userDetails.getUser().getRole();
+        String accessToken = createToken(username, role);
+        String refreshToken = generateRefreshToken();
+        tokenStore.storeRefreshToken(username, refreshToken);
+        return new Tokens(accessToken, refreshToken);
+    }
 
-        return createToken(username, role);
+    public String generateRefreshToken(Long kakaoId) {
+        Date date = new Date();
+        return BEARER_PREFIX +
+            Jwts.builder()
+                .setId(UUID.randomUUID().toString())
+                .claim("kakao_id", kakaoId)
+                .setIssuedAt(date)
+                .setExpiration(new Date(date.getTime() + REFRESH_TOKEN_EXPIRATION_TIME))
+                .signWith(refreshKey, refreshSignatureAlgorithm)
+                .compact();
     }
 }
